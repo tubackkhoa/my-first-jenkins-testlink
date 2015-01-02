5b6ae14f8cd7a84436b93befed4ddb56a77bf688
gitprep <gitprep@example.com>
2015-01-03 01:39:15 +0700

    change

--- src/test/java/jenkins/plugins/testlink/examples/TestCurrentTime.java
+++ src/test/java/jenkins/plugins/testlink/examples/TestCurrentTime.java
@@ -40,7 +40,7 @@ public class TestCurrentTime
 	{
 		Assert.assertNotNull( System.currentTimeMillis() );
 		
-		Assert.assertTrue( System.currentTimeMillis() > 0 );
+		Assert.assertTrue( System.currentTimeMillis() > 1 );
 	}
 	
 }
